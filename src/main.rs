24767e267a4a7d40478b2c330d9c43c9a67112d1
UtsavBalar1231 <utsavbalar1231@gmail.com>
2023-09-01 14:12:39 +0530

    todo-git-rs: Implement spinner animation feature
    Change-Id: I30999335f7c03b2e7ca69aa15a554b1fb0a977f9
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use std::io::{Read, Write};
+use spinoff::{spinners, Color, Spinner};
+use std::io::{self, Read, Write};
 use todo_git::{issue::Issue, todo_git::TodoGit, LATEST_ISSUE};
 mod args;
-use std::io;
 
 // TODO: Implement HTTP Response error handling
 // TODO: Implement Multi todo-list issues handling
@@ -13,17 +13,25 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     match cli.subcommand() {
         Some(("edit", _argmatches)) => {
+            let mut sp = Spinner::new(spinners::Dots2, "Fetching latest issue", Color::Yellow);
             let client = reqwest::Client::new();
             let issues = Issue::get_issues_list(&client).await?;
 
             let issue_number = issues.get(LATEST_ISSUE).expect("No issues found!");
 
             if let Some(body) = &issue_number.body {
+                sp.update(
+                    spinners::Dots2,
+                    "Successfully fetched latest issue",
+                    Color::Green,
+                );
                 const TMP_FILE: &str = "/tmp/todo-git.md";
                 let mut tmp_todofile = std::fs::File::create(TMP_FILE)?;
                 tmp_todofile.write_all(body.as_bytes())?;
 
-                let editor = option_env!("EDITOR").unwrap_or_else(|| "vim");
+                let editor = option_env!("EDITOR").unwrap_or("vim");
+
+                sp.success(&format!("Opening issue body in {}", editor));
 
                 let status = std::process::Command::new(editor)
                     .arg(TMP_FILE)
@@ -31,9 +39,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                     .expect("Error opening editor!");
 
                 if !status.success() {
-                    println!("Error in opening editor!");
+                    sp.fail("Error in opening editor!");
                     std::process::exit(1);
                 }
+                let mut sp = Spinner::new(spinners::Dots2, "Updating issue body", Color::Yellow);
 
                 let mut tmp_todofile = std::fs::File::open(TMP_FILE)?;
                 let mut body = String::new();
@@ -42,12 +51,15 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 let update_issue = issue_number.update_issue(&client, &body).await?;
 
                 if !update_issue.status().is_success() {
-                    println!("Error occurred while updating issue!");
+                    sp.fail("Error occurred while updating issue!");
                     std::process::exit(1);
                 }
+
+                sp.success("Successfully updated issue body!");
             }
         }
         Some(("view", _argmatches)) => {
+            let mut sp = Spinner::new(spinners::Dots2, "Fetching latest issue", Color::Yellow);
             let client = reqwest::Client::new();
             let issues = Issue::get_issues_list(&client).await?;
 
@@ -55,10 +67,14 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
             if let Some(body) = &issue_number.body {
                 println!("{body}");
+                sp.success("Successfully printed the issue content");
+            } else {
+                sp.success("Empty issue body");
             }
         }
 
         Some(("delete", _argmatches)) => {
+            let mut sp = Spinner::new(spinners::Dots2, "Deleting latest issue", Color::Yellow);
             let client = reqwest::Client::new();
             let issues = Issue::get_issues_list(&client).await?;
 
@@ -67,50 +83,58 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             let response = issue_number.delete_issue(&client).await?;
 
             if !response.status().is_success() {
-                panic!("Failed to delete issue!: {}", response.status());
+                sp.fail(&format!("Failed to delete issue!: {}", response.status()));
             }
 
-            println!(
+            sp.success(&format!(
                 "Issue {} deleted from Github repo",
                 issue_number.number.unwrap()
-            );
+            ));
         }
 
         Some(("create-config", argmatches)) => {
+            let mut sp = Spinner::new(spinners::Dots2, "Creating config file", Color::Yellow);
             let mut config = TodoGit::default();
             let todo_config = format!("{}/.{}.json", env!("HOME"), env!("CARGO_PKG_NAME"),);
             let config_path = std::path::PathBuf::from(&todo_config);
 
             if config_path.exists() {
-                println!("An config file already exists at {}", config_path.display());
+                sp.fail(&format!(
+                    "An config file already exists at {}",
+                    config_path.display()
+                ));
                 std::process::exit(1);
             }
 
             if argmatches.contains_id("interactive") {
-                println!();
-                print!("Enter Github owner: ");
+                sp.stop();
+                println!("Enter Github owner: ");
                 io::stdin()
                     .read_line(&mut config.owner)
                     .expect("Failed to read owner");
-                print!("\nEnter Github repository: ");
+                println!("Enter Github repository: ");
                 io::stdin()
                     .read_line(&mut config.repo)
                     .expect("Failed to read repo");
-                print!("\nEnter Owner's Github token:");
+                println!("Enter Owner's Github token:");
                 io::stdin()
                     .read_line(&mut config.token)
                     .expect("Failed to read token");
                 println!();
             } else {
-                println!("Edit the {} file", config_path.display());
+                sp.success(&format!("Edit the {} file", config_path.display()));
             }
+            let mut sp = Spinner::new(spinners::Dots2, "Saving config file", Color::Yellow);
 
             let config = serde_json::to_string_pretty(&config).expect("Failed to create config");
 
             std::fs::write(&config_path, config)?;
+
+            sp.success("Successfully created config file");
         }
 
         Some(("new", argmatches)) => {
+            let mut sp = Spinner::new(spinners::Dots2, "Creating new issue", Color::Yellow);
             let client = reqwest::Client::new();
 
             let mut title_arg: Option<&str> = None;
@@ -126,11 +150,14 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             let new_issue = Issue::create_new(&client, title_arg, body_arg).await?;
 
             if !new_issue.status().is_success() {
-                println!("Failed to create new issue: {}", new_issue.status());
+                sp.warn(&format!(
+                    "Failed to create new issue: {}",
+                    new_issue.status()
+                ));
                 std::process::exit(1);
             }
 
-            println!("Successfully created new issue");
+            sp.success("Successfully created new issue");
         }
 
         _ => {}
